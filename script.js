42d0f3103e7843b810ebeb2fd33c8860311a363b
debreroha <debreroha12@gmail.com>
2023-09-22 13:16:22 +0300

    last game

--- script.js
+++ script.js
@@ -97,5 +97,40 @@ for (const odd of odds) {
 average /= odds.length;
 console.log(average);
 
+const scorers = {};
+for (const player of game.scored) {
+  scorers[player] ? scorers[player]++ : (scorers[player] = 1);
+}
+console.log(scorers);
 ///////////////////////////////////////////////////////////////////////
 // coding challenge 4
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+// 1.
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+// 2.
+console.log(gameEvents.delete(64));
+console.log(gameEvents);
+
+// 3.
+console.log('An event happened, on average, every 9 minutes');
+
+// 4.
+for (const [min, event] of gameEvents) {
+  const half = min <= 45 ? 'FIRST' : 'SECOND';
+  console.log(`[${half} HALF] ${min}: ${event}`);
+}
