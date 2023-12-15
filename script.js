fd1416f53b7e490ff2364a4ed630ac398b02365c
Getnet Alemye <124487134+debreroha@users.noreply.github.com>
2023-12-15 17:27:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -76,7 +76,7 @@ printGoals(...game.scored);
 team1 < team2 && console.log('Team 1 is more likely to win!');
 team1 > team2 && console.log('Team 2 is more likely to win!');
 
-///////////////////////////////////////
+///////////////////////
 // coding challenge 3
 // 1.
 for (const [x, y] of game.scored.entries()) {
